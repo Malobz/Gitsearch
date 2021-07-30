79abf926985dea5617dd2c114d6ce6c4c98bb420
Mercy Maloba <mercy.maloba@student.moringaschool.com>
2021-07-30 23:41:04 +0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router'; 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
